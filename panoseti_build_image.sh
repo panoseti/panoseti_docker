68e50c3d9089de2d225d2fd47539e6aed7acc21c
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-03-23 16:30:52 -0700

    add panoseti-bridge for network.

--- panoseti_build_image.sh
+++ panoseti_build_image.sh
@@ -6,3 +6,4 @@ sudo docker pull grafana/grafana
 sudo docker pull defreitas/dns-proxy-server
 sudo docker rmi panoseti_control:latest
 sudo docker build -t panoseti_control:latest .
+sudo docker network create panoseti-bridge
--- panoseti_create_containers.sh
+++ panoseti_create_containers.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-sudo docker run -itd --name panoseti-control --hostname panoseti-control --device=/dev/ttyUSB5 -v /home/wei/headnode_data:/home/data panoseti_control:latest
-sudo docker run -d --name panoseti-redis --hostname panoseti-redis -v /home/wei/database/redis:/data redis redis-server --save 60 1 --loglevel warning
-sudo docker run -d --name panoseti-influxdb --hostname panoseti-influxdb -v /home/wei/database/influxdb:/var/lib/influxdb influxdb:1.8.10
-sudo docker run -d --name panoseti-grafana --hostname panoseti-grafana grafana/grafana
-sudo docker run -itd --name panoseti-dns --hostname panoseti-dns -v /var/run/docker.sock:/var/run/docker.sock -v /etc/resolv.conf:/etc/resolv.conf defreitas/dns-proxy-server
+sudo docker run -itd --name panoseti-control --hostname panoseti-control --net=panoseti-bridge --device=/dev/ttyUSB5 -v /home/wei/headnode_data:/home/data panoseti_control:latest
+sudo docker run -d --name panoseti-redis --hostname panoseti-redis --net=panoseti-bridge -v /home/wei/database/redis:/data redis redis-server --save 60 1 --loglevel warning
+sudo docker run -d --name panoseti-influxdb --hostname panoseti-influxdb --net=panoseti-bridge -v /home/wei/database/influxdb:/var/lib/influxdb influxdb:1.8.10
+sudo docker run -d --name panoseti-grafana --hostname panoseti-grafana --net=panoseti-bridge grafana/grafana
+sudo docker run -itd --name panoseti-dns --hostname panoseti-dns --net=panoseti-bridge -v /var/run/docker.sock:/var/run/docker.sock -v /etc/resolv.conf:/etc/resolv.conf defreitas/dns-proxy-server
