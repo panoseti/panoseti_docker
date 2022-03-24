252a1f933d1ce95c39d049860f402380f0eec960
Wei Liu <liuwei_berkeley@berkeley.edu>
2022-03-23 23:03:48 -0700

    grfana data is in really persistent storage now!

--- panoseti_create_containers.sh
+++ panoseti_create_containers.sh
@@ -5,8 +5,9 @@
 # 2. the device name of gps receiver should be a variable
 # 3. the IP address of WRS should be a variable
 
+ID=$(id -u)
 sudo docker run -itd --name panoseti-control --hostname panoseti-control --net=panoseti-bridge --device=/dev/ttyUSB5 -v /home/wei/headnode_data:/home/data panoseti_control:latest
 sudo docker run -d --name panoseti-redis --hostname panoseti-redis --net=panoseti-bridge -v /home/wei/database/redis:/data redis redis-server --save 60 1 --loglevel warning
 sudo docker run -d --name panoseti-influxdb --hostname panoseti-influxdb --net=panoseti-bridge -v /home/wei/database/influxdb:/var/lib/influxdb influxdb:1.8.10
-sudo docker run -d --name panoseti-grafana --hostname panoseti-grafana --net=panoseti-bridge -v /home/wei/database/grafana:/var/lib/grafana grafana/grafana
+sudo docker run -d --name panoseti-grafana --hostname panoseti-grafana --net=panoseti-bridge  --user $ID -v /home/wei/database/grafana:/var/lib/grafana grafana/grafana
 sudo docker run -itd --name panoseti-dns --hostname panoseti-dns --net=panoseti-bridge -v /var/run/docker.sock:/var/run/docker.sock -v /etc/resolv.conf:/etc/resolv.conf defreitas/dns-proxy-server
