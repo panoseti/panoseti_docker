ca8337cededcbba63a2582bbc402aa0714f78c52
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-06-06 11:03:42 -0700

    Modify panoseti_build_image.sh and panoseti_create_containers.sh to include custom dashboard configuration. Add the folder panoseti_grafana which:specifies the dashboard configuration and how to build grafana image.

--- panoseti_build_image.sh
+++ panoseti_build_image.sh
@@ -2,7 +2,8 @@
 
 sudo docker pull redis
 sudo docker pull influxdb:1.8.10
-sudo docker pull grafana/grafana
+sudo docker rmi panoseti_grafana:latest
+sudo docker build -t panoseti_grafana:latest panoseti_grafana/
 sudo docker pull defreitas/dns-proxy-server
 sudo docker rmi panoseti_control:latest
 sudo docker build -t panoseti_control:latest .
--- panoseti_create_containers.sh
+++ panoseti_create_containers.sh
@@ -18,5 +18,5 @@ ID=$(id -u)
 sudo docker run -itd --name panoseti-control --hostname panoseti-control --net=panoseti-bridge -p $HK_PORT:$HK_PORT/udp --privileged -u root -v $DATA_PATH/headnode_data:/home/data panoseti_control:latest
 sudo docker run -d --name panoseti-redis --hostname panoseti-redis --net=panoseti-bridge -v $DATA_PATH/database/redis:/data redis redis-server --save 60 1 --loglevel warning
 sudo docker run -d --name panoseti-influxdb --hostname panoseti-influxdb --net=panoseti-bridge -v $DATA_PATH/database/influxdb:/var/lib/influxdb influxdb:1.8.10
-sudo docker run -d --name panoseti-grafana --hostname panoseti-grafana --net=panoseti-bridge  --user $ID -v $DATA_PATH/database/grafana:/var/lib/grafana grafana/grafana
+sudo docker run -d --name panoseti-grafana --hostname panoseti-grafana --net=panoseti-bridge  --user $ID -v $DATA_PATH/database/grafana:/var/lib/grafana panoseti_grafana:latest
 sudo docker run -itd --name panoseti-dns --hostname panoseti-dns --net=panoseti-bridge -v /var/run/docker.sock:/var/run/docker.sock -v /etc/resolv.conf:/etc/resolv.conf defreitas/dns-proxy-server
