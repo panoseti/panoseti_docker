14f2f3f802cdbb2adba4861d5e2c7d0449f88084
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2022-06-06 11:33:24 -0700

    Revert "Add custom Grafana docker build"

--- panoseti_build_image.sh
+++ panoseti_build_image.sh
@@ -2,8 +2,7 @@
 
 sudo docker pull redis
 sudo docker pull influxdb:1.8.10
-sudo docker rmi panoseti_grafana:latest
-sudo docker build -t panoseti_grafana:latest panoseti_grafana/
+sudo docker pull grafana/grafana
 sudo docker pull defreitas/dns-proxy-server
 sudo docker rmi panoseti_control:latest
 sudo docker build -t panoseti_control:latest .
